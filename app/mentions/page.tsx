b6fb66698a7c9df48ac4ff0f1c23ce70090064f1
Arnaudb78 <arnaudb92230@gmail.com>
2024-07-26 11:48:16 +0200

    FIX : problems with accent

--- app/mentions/page.tsx
+++ app/mentions/page.tsx
@@ -10,36 +10,36 @@ export default function Mentions() {
             id: 3,
             title: "Propriété intellectuelle",
             content:
-                "L'ensemble des éléments constituant le site (textes, images, graphismes, logos, vidéos, sons, etc.) est protégé par la législation française sur le droit d'auteur et la propriété intellectuelle. Toute reproduction, représentation, modification, publication, adaptation de tout ou partie des éléments du site, quel que soit le moyen ou le procédé utilisé, est interdite, sauf autorisation écrite préalable de l'éditeur.",
+                "L&apos;ensemble des éléments constituant le site (textes, images, graphismes, logos, vidéos, sons, etc.) est protégé par la législation française sur le droit d&apos;auteur et la propriété intellectuelle. Toute reproduction, représentation, modification, publication, adaptation de tout ou partie des éléments du site, quel que soit le moyen ou le procédé utilisé, est interdite, sauf autorisation écrite préalable de l&apos;éditeur.",
         },
         {
             id: 4,
             title: "Données personnelles",
             content:
-                "Les informations recueillies sur le site Urban-Roots sont enregistrées dans un fichier informatisé par Arnaud Beaulieu pour analyser les données. Elles sont conservées pendant une durée de 2ans et sont destinées à être analyser pour des fins d'amélioration de notre solution. Conformément à la loi « informatique et libertés », vous pouvez exercer votre droit d'accès aux données vous concernant et les faire rectifier en contactant : arnaudb92230@gmail.com.",
+                "Les informations recueillies sur le site Urban-Roots sont enregistrées dans un fichier informatisé par Arnaud Beaulieu pour analyser les données. Elles sont conservées pendant une durée de 2ans et sont destinées à être analyser pour des fins d&apos;amélioration de notre solution. Conformément à la loi « informatique et libertés », vous pouvez exercer votre droit d&apos;accès aux données vous concernant et les faire rectifier en contactant : arnaudb92230@gmail.com.",
         },
         {
             id: 5,
             title: "Cookies",
             content:
-                "Le site Urban-Roots utilise des cookies pour améliorer l'expérience utilisateur et à des fins de statistiques. Vous pouvez configurer votre navigateur pour refuser tous les cookies ou pour être averti lorsque des cookies sont envoyés. Cependant, certaines fonctionnalités du site pourraient ne pas fonctionner correctement sans cookies.",
+                "Le site Urban-Roots utilise des cookies pour améliorer l&apos;expérience utilisateur et à des fins de statistiques. Vous pouvez configurer votre navigateur pour refuser tous les cookies ou pour être averti lorsque des cookies sont envoyés. Cependant, certaines fonctionnalités du site pourraient ne pas fonctionner correctement sans cookies.",
         },
         {
             id: 6,
             title: "Limitation de responsabilité",
             content:
-                "Urban-Roots s'efforce de fournir des informations aussi précises que possible sur le site. Toutefois, il ne saurait être tenu responsable des omissions, inexactitudes et carences dans la mise à jour, qu'elles soient de son fait ou du fait des tiers partenaires qui lui fournissent ces informations. L'utilisateur du site Urban-Roots reconnaît disposer de la compétence et des moyens nécessaires pour accéder et utiliser ce site.",
+                "Urban-Roots s&apos;efforce de fournir des informations aussi précises que possible sur le site. Toutefois, il ne saurait être tenu responsable des omissions, inexactitudes et carences dans la mise à jour, qu&apos;elles soient de son fait ou du fait des tiers partenaires qui lui fournissent ces informations. L&apos;utilisateur du site Urban-Roots reconnaît disposer de la compétence et des moyens nécessaires pour accéder et utiliser ce site.",
         },
         {
             id: 7,
             title: "Droit applicable et attribution de juridiction",
             content:
-                "Tout litige en relation avec l'utilisation du site Urban-Roots est soumis au droit français. Il est fait attribution exclusive de juridiction aux tribunaux compétents de Paris.",
+                "Tout litige en relation avec l&apos;utilisation du site Urban-Roots est soumis au droit français. Il est fait attribution exclusive de juridiction aux tribunaux compétents de Paris.",
         },
         {
             id: 8,
             title: "Contact",
-            content: "Pour toute question ou réclamation, veuillez nous contacter à l'adresse suivante : arnaudb92230@gmail.com.",
+            content: "Pour toute question ou réclamation, veuillez nous contacter à l&apos;adresse suivante : arnaudb92230@gmail.com.",
         },
     ];
 
@@ -80,10 +80,10 @@ export default function Mentions() {
                             </a>
                         </p>
                         <p>
-                            <span className="font-bold">Adresse de l'hébergeur :</span> -
+                            <span className="font-bold">Adresse de l&apos;hébergeur :</span> -
                         </p>
                         <p>
-                            <span className="font-bold">Numéros de téléphone de l'hébergeur :</span> -
+                            <span className="font-bold">Numéros de téléphone de l&apos;hébergeur :</span> -
                         </p>
                     </div>
                     {mentions}
