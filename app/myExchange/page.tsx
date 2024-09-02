7b4f029cd728554f28b7588b4105d712f76bbf8e
Arnaudb78 <arnaudb92230@gmail.com>
2024-09-02 22:32:20 +0200

    FIX: problem with function myExchange

--- app/myExchange/page.tsx
+++ app/myExchange/page.tsx
@@ -6,7 +6,7 @@ import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useRouter } from "next/navigation";
 
-export default function myExchange() {
+export default function MyExchange() {
     const router = useRouter();
 
     function handleBack() {
