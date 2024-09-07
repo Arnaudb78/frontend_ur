082240694b916c91153c67971af18e989e615bae
Arnaudb78 <arnaudb92230@gmail.com>
2024-09-07 15:51:22 +0200

    DONE: responsive done

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -9,6 +9,9 @@ export default function MapPage() {
         <>
             <Navbar />
             <section className="p-6  w-full h-full bg-secondary-100 pt-24 sm:p-8 sm:pt-24 md:p-10 md:pt-28 lg:pt-30 xl:pt-36 xl:p-20">
+                <p className="text-xl text-center font-bold md:text-2xl lg:text-3xl xl:text-4xl">
+                        Ici, retrouve tous les <span className="bg-secondary-300 text-secondary-100 inline-block rotate-3">Jardin-Potager</span> disponibles.
+                    </p>
                 <Map />
             </section>
             <Footer />
